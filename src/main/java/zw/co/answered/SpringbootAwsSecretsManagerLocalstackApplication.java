c0202b48f3515cf4fc5d87b75d9d4bb8a5a152ca
anesumushate <anesu.mushate@yoyogroup.com>
2025-01-30 11:35:23 +0200

    base project commit

---
+++ src/main/java/zw/co/answered/SpringbootAwsSecretsManagerLocalstackApplication.java
@@ -0,0 +1,13 @@
+package zw.co.answered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsSecretsManagerLocalstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsSecretsManagerLocalstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zw/co/answered/SpringbootAwsSecretsManagerLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.answered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsSecretsManagerLocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
